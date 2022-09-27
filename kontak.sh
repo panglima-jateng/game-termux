74166c7a1e62e4c6a2e2d6da7d610caf43882a7f
Diki Andreano <92381721+panglima-jateng@users.noreply.github.com>
2022-09-27 14:16:29 +0700

    Add files via upload

---
+++ kontak.sh
@@ -0,0 +1,277 @@
+#!/bin/bash
+# data bocor kontak whatsapp
+# code warna
+cyan='\e[0;36m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+nc='\e[0m'
+
+clear
+sleep 1
+toilet -f big -F metal HACK NW
+echo
+echo -e "           $cyan   [ $darkgray anjay hengker $cyan ]"
+echo -e $lightred"÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷"
+echo -e $cyan" {$brown•$cyan} $darkgray Author $brown : $darkgray Panglima Jateng"
+echo -e $cyan" {$brown•$cyan} $darkgray Team   $brown : $darkgray Mafia Teknologi"
+echo -e $cyan" {$brown•$cyan} $darkgray Bloger $brown : $darkgray diki-andre.blogspot.com"
+echo -e $lightred"÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷"
+sleep 1
+     echo ""
+     echo -e "    $cyan     ($brown+$cyan)$darkgray Menu$cyan ($brown+$cyan)"
+     echo -e $purple"================================"
+     echo -e $purple"=  $cyan  {$brown 1 $cyan}$darkgray Subcribe Autor $purple     ="
+     echo -e $purple"=  $cyan  {$brown 2 $cyan}$darkgray Nomor Cewek 1  $purple     ="
+     echo -e $purple"=  $cyan  {$brown 3 $cyan}$darkgray Nomor Cewek 2  $purple     ="
+     echo -e $purple"=  $cyan  {$brown 4 $cyan}$darkgray Keluar         $purple     ="
+     echo -e $purple"=  $cyan  {$brown 0 $cyan}$darkgray grup wa        $purple     ="
+     echo -e $purple"================================"
+sleep 1
+echo -e " $lightgreen	"
+read -p "===> " nomber
+if [[ $nomber == '1' ]];then
+ xdg-open https://youtube.com/channel/UCSJohuQCtqfD2P73Z65g8jg
+   sleep 2
+     bash kontak.sh
+fi
+if [[ $nomber == '2' ]];then
+echo
+sleep 1
+     echo -e " amelia = +62 838-6348-6501"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " firda  = +62 857-4757-1147"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " putri  = +62 859-3856-8681"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " lulu   = +62 812-7849-0875"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " mayang = +62 831-9845-2419"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " nopi   = +62 882-1210-7481"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " nurul  = +62 888-6571-225"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " rahma  = +62 895-6165-94030"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " salsa  = +62 838-9624-7081"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " shila  = +62 882-0063-81233"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " yuni   = +62 882-2007-8226"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " nur    = 082311455485"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " carina = 085321606820"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " agita  = 085321606820"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " putry  = 083898077499"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " yenika = 082187748875"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " nata   = 082311455485"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " bina   = 085242557186"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " lina   = 085315470011"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " fitri  = 087772216729"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " nifa   = 0895335180271"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " diana  = 082317277118"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " ayu    = 082316538570"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " puput  = 085813436735"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " yuvita = 085335076294"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " dewi   = 085335019635"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Indah  = 0853 7637 9373"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Gita   = 0898 7323 1938"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Ryani  = 0821 3217 3472"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Tyara  = 0832 0823 4923"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Rana   = 0812 2384 8433"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Intan  = 0889 3746 9384"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Sinta  = 0888 34221 3423"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Riana  = 0812 3181 3989"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Mirah  = 0812 2382 3549"
+     echo -e "-----------------------------------------------"
+sleep 1
+     echo -e " Amel   = 0823 3478 1414"
+     echo -e "-----------------------------------------------"
+fi
+if [[ $nomber == '3' ]];then
+sleep 2
+   sleep 0
+        echo "+62 831-9592-6669"
+sleep 0
+        echo "+62 853-4171-7106"
+sleep 0
+        echo "+62 812-8612-2979"
+sleep 0
+        echo "+62 856-0053-6814"
+sleep 0
+        echo "+62 858-0830-3642"
+sleep 0
+        echo "+62 858-5059-9149"
+sleep 0
+        echo "+62 857-3336-3979"
+sleep 0
+        echo "+62 852-3617-0356"
+sleep 0
+        echo "+62 821-1842-6491"
+sleep 0
+        echo "+62 857-7917-5576"
+sleep 0
+        echo "+62 881-0371-00206"
+sleep 0
+        echo "+62 821-6711-7720"
+sleep 0
+        echo "+62 821-4078-9230"
+sleep 0
+        echo "+62 822-7415-8207"
+sleep 0
+        echo "+62 812-2576-7437"
+sleep 0
+        echo "+62 813-4549-4791"
+sleep 0
+        echo "+62 831-0448-6267"
+sleep 0
+        echo "+62 822-7757-6785"
+sleep 0
+        echo "+62 852-9871-1321"
+sleep 0
+        echo "+62 821-7169-4128"
+sleep 0
+        echo "+62 877-8562-7762"
+sleep 0
+        echo "+62 896-5319-2287"
+sleep 0
+        echo "+62 852-3060-9196"
+sleep 0
+        echo "+62 822-8462-3351"
+sleep 0
+        echo "+62 815-2572-3170"
+sleep 0
+        echo "+62 882-6810-0243"
+sleep 0
+        echo "+62 821-8152-5846"
+sleep 0
+        echo "+62 895-3363-91063"
+sleep 0
+        echo "+62 857-9418-8178"
+sleep 0
+        echo "+62 859-1804-11835"
+sleep 0
+        echo "+62 831-2870-0003"
+sleep 0
+        echo "+62 852-8154-8798"
+sleep 0
+        echo "+62 895-3203-85608"
+sleep 0
+        echo "+62 881-7603-107"
+sleep 0
+        echo "+62 813-9175-6710"
+sleep 0
+        echo "+62 882-1697-3416"
+sleep 0
+        echo "+62 823-0129-8429"
+sleep 0
+        echo "+62 878-3712-4666"
+sleep 0
+        echo "+63 821-7172-4223"
+sleep 0
+        echo "0852-6697-0204"
+sleep 0
+        echo "0857-7267-6495"
+sleep 0
+        echo "0819-4770-2853"
+sleep 0
+        echo "0858-8849-7547"
+sleep 0
+        echo "0813-4710-7033"
+sleep 0
+        echo "+62 813-8201-1190"
+sleep 0
+        echo "0896-2807-7388"
+sleep 0
+        echo "085335019580"
+sleep 0
+        echo "082258896755"
+sleep 0
+        echo "085732055041"
+sleep 0
+        echo "082394542812"
+sleep 0
+        echo "085314268899"
+sleep 0
+        echo "083112834549"
+sleep 0
+        echo "082350390359"
+fi
+if [[ $nomber == '4' ]];then
+exit
+logout
+
+fi
+if [[ $nomber == '0' ]];then
+clear
+sleep 1
+figlet welcome | lolcat
+sleep 2
+       xdg-open https://chat.whatsapp.com/KDi54hngTNl8hUxD5CiGUL
+fi
